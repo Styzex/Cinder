ab72d4a5e4456893e552c331939d347465d8661b
Viktor Paraj <viktor.paraj@gmail.com>
2026-08-27 19:26:18 +0200

    gitignore

--- obj/Debug/net10.0/Cinder.AssemblyInfo.cs
+++ obj/Debug/net10.0/Cinder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a8fba2a3a54b43f30f90daa422ac694a6a0591")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
